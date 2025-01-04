a202673be82c891964823ca08de5219d17315239
rajatnm <x22150927@student.ncirl.ie>
2025-01-04 00:17:36 +0000

    update

---
+++ src/main/java/com/example/recipecrud/rest/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.recipecrud.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Recipe Maker App!";
+    }
+}
